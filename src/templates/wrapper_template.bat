83788e9a6d4213aeaff01e9aa879fdb0799ce62d
magynhard <github@mail.magynhard.de>
2024-05-11 23:43:57 +0200

    add space to fix compatibility problems in some windows cases, where the first character is ommitted

--- src/templates/wrapper_template.bat
+++ src/templates/wrapper_template.bat
@@ -1,43 +1,43 @@
-@echo off
-REM use UTF8
-chcp 65001 > NUL
+ @echo off
+ REM use UTF8
+ chcp 65001 > NUL
 
-set "ORIGINAL_HTTP_PROXY=%HTTP_PROXY%"
-set "ORIGINAL_HTTPS_PROXY=%HTTPS_PROXY%"
-set "ORIGINAL_RUBYPATH=%RUBYPATH%"
+ SET "ORIGINAL_HTTP_PROXY=%HTTP_PROXY%"
+ SET "ORIGINAL_HTTPS_PROXY=%HTTPS_PROXY%"
+ SET "ORIGINAL_RUBYPATH=%RUBYPATH%"
 
-set "NODE_JS_RUNTIME_PATH={{node_js_runtime_path}}"
-set "RVM_ROOT_PATH={{rvm_root_dir}}"
+ SET "NODE_JS_RUNTIME_PATH={{node_js_runtime_path}}"
+ SET "RVM_ROOT_PATH={{rvm_root_dir}}"
 
-REM PATH: prepend wrapper
-for /f "delims=" %%p in ('%NODE_JS_RUNTIME_PATH% "%RVM_ROOT_PATH%\src\wrapper\wrapper_path.js" "%CD%" %~n0') do set "return_path=%%p"
-REM echo Returned path: "%return_path%"
-call %return_path%
+ REM PATH: prepend wrapper
+ for /f "delims=" %%p in ('%NODE_JS_RUNTIME_PATH% "%RVM_ROOT_PATH%\src\wrapper\wrapper_path.js" "%CD%" %~n0') do SET "return_path=%%p"
+ REM echo Returned path: "%return_path%"
+ call %return_path%
 
-REM RUBYPATH
-for /f "delims=" %%a in ('%NODE_JS_RUNTIME_PATH% "%RVM_ROOT_PATH%\src\wrapper\wrapper_rubypath.js" "%CD%" %~n0') do set "return_rubypath=%%a"
-REM echo Returned path: "%return_rubypath%"
-call %return_rubypath%
+ REM RUBYPATH
+ for /f "delims=" %%a in ('%NODE_JS_RUNTIME_PATH% "%RVM_ROOT_PATH%\src\wrapper\wrapper_rubypath.js" "%CD%" %~n0') do SET "return_rubypath=%%a"
+ REM echo Returned path: "%return_rubypath%"
+ call %return_rubypath%
 
-REM PROXY
-for /f "delims=" %%b in ('%NODE_JS_RUNTIME_PATH% "%RVM_ROOT_PATH%\src\wrapper\wrapper_proxy.js" "%CD%" %~n0') do set "return_proxy=%%b"
-REM echo Returned proxy: "%return_proxy%"
-if not "%return_proxy%" == "" (
-	set "HTTP_PROXY=%return_proxy%"
-	set "HTTPS_PROXY=%return_proxy%"
-	set "http_proxy=%return_proxy%"
-	set "https_proxy=%return_proxy%"
-)
+ REM PROXY
+ for /f "delims=" %%b in ('%NODE_JS_RUNTIME_PATH% "%RVM_ROOT_PATH%\src\wrapper\wrapper_proxy.js" "%CD%" %~n0') do SET "return_proxy=%%b"
+ REM echo Returned proxy: "%return_proxy%"
+ if not "%return_proxy%" == "" (
+ 	SET "HTTP_PROXY=%return_proxy%"
+ 	SET "HTTPS_PROXY=%return_proxy%"
+ 	SET "http_proxy=%return_proxy%"
+ 	SET "https_proxy=%return_proxy%"
+ )
 
-REM FINAL COMMAND
-for /f "delims=" %%c in ('%NODE_JS_RUNTIME_PATH% "%RVM_ROOT_PATH%\src\wrapper\wrapper_runner.js" "%CD%" %~n0') do set "return_value=%%c"
-REM echo Returned value: "%return_value%"
-call %return_value% %*
+ REM FINAL COMMAND
+ for /f "delims=" %%c in ('%NODE_JS_RUNTIME_PATH% "%RVM_ROOT_PATH%\src\wrapper\wrapper_runner.js" "%CD%" %~n0') do SET "return_value=%%c"
+ REM echo Returned value: "%return_value%"
+ CALL %return_value% %*
 
-REM reset variables
-set "HTTP_PROXY=%ORIGINAL_HTTP_PROXY%"
-set "HTTPS_PROXY=%ORIGINAL_HTTPS_PROXY%"
-set "RUBYPATH=%ORIGINAL_RUBYPATH%"
+ REM reSET variables
+ SET "HTTP_PROXY=%ORIGINAL_HTTP_PROXY%"
+ SET "HTTPS_PROXY=%ORIGINAL_HTTPS_PROXY%"
+ SET "RUBYPATH=%ORIGINAL_RUBYPATH%"
 
-REM AFTER COMMAND
-%NODE_JS_RUNTIME_PATH% "%RVM_ROOT_PATH%\src\wrapper\wrapper_after.js" "%CD%" %~n0
+ REM AFTER COMMAND
+ %NODE_JS_RUNTIME_PATH% "%RVM_ROOT_PATH%\src\wrapper\wrapper_after.js" "%CD%" %~n0
