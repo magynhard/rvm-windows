67cd01d23f1744dc3ac10fcfea908e343b38a93e
magynhard <github@mail.magynhard.de>
2024-05-12 19:00:39 +0200

    fix scan if no ruby is found in path

--- src/cli/tasks/_scan.js
+++ src/cli/tasks/_scan.js
@@ -25,27 +25,28 @@ class RvmCliScan {
      */
     static scanMissingEnvironmentPaths() {
         const self = RvmCliScan;
+        let paths = [];
         try {
             let paths = execSync(`chcp 65001 > NUL & where ruby`, { stdio: 'pipe'}).toString();
             paths = paths.split("\n");
-            const rvm_paths = Dir.glob(RvmCliTools.getRvmDataDir() + '/envs/*');
             paths = paths.concat(rvm_paths);
-            paths.concat(Dir.glob("C:\\Program Files\\Ruby*"));
-            paths.concat(Dir.glob("C:\\Ruby*"));
-            let new_config = RvmCliTools.config();
-            paths.eachWithIndex((path, i) => {
-                if(path) {
-                    path = File.normalizePath(path.trim());
-                    path = path.replace("/bin/ruby.exe", "");
-                    if(!Object.values(new_config).includes(path)) {
-                        new_config.envs["unknown_" + i] = File.normalizePath(path);
-                    }
-                }
-            });
-            RvmCliTools.writeRvmConfig(new_config);
         } catch (e) {
-            // no installed ruby available
+            // no installed ruby in PATH available
         }
+        paths = paths.concat(Dir.glob("C:\\Program Files\\Ruby*"));
+        paths = paths.concat(Dir.glob("C:\\Ruby*"));
+        paths = paths.concat(Dir.glob(RvmCliTools.getRvmDataDir() + '/envs/*'));
+        let new_config = RvmCliTools.config();
+        paths.eachWithIndex((path, i) => {
+            if(path) {
+                path = File.normalizePath(path.trim());
+                path = path.replace("/bin/ruby.exe", "");
+                if(!Object.values(new_config).includes(path)) {
+                    new_config.envs["unknown_" + i] = File.normalizePath(path);
+                }
+            }
+        });
+        RvmCliTools.writeRvmConfig(new_config);
     }
 }
 
